35306ddf7c022b9dae50bdbb245759dfe04a2e42
Henry <94872110+Henryking98@users.noreply.github.com>
2025-09-26 01:46:47 +0100

    feat: implemented homepage ui

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,25 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode } from 'react';
+import Link from "next/link";
+import Image from "next/image";
 
-const RootLayout = ({children}: {children: ReactNode}) => {
-  return (
-    <div>{children}</div>
-  )
-}
+const RootLayout = ({ children }: { children: ReactNode }) => {
+    return (
+        <div className="root-layout">
+            <nav>
+                <Link href="/" className="flex items-center gap-2">
+                    <Image
+                        src="/logo.svg"
+                        alt="MockMate Logo"
+                        width={38}
+                        height={32}
+                    />
+                    <h2 className="text-primary-100">PrepWise</h2>
+                </Link>
+            </nav>
 
-export default RootLayout
+            {children}
+        </div>
+    );
+};
+
+export default RootLayout;
